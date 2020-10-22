756be4bd2c3bbcd4ed7a86c6ceb0b0a09d29d157
FrancescoViterbo <franc.viterbo@gmail.com>
2020-10-22 14:46:50 +0200

    migliorato grafica, aggiunto pulsante per tirare il dado e selettori js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 /* Selettori */
 const loggedOrNot = document.getElementById("logged-or-not");
+const rollingDice = document.getElementById("rolling-dice");
+const showNum = document.getElementById("show-num");
+const muResult = document.getElementById("my-result");
+const ShowNum2 = document.getElementById("show-num-2");
+const pcResult = document.getElementById("pc-result");
 
 
 
@@ -9,17 +14,20 @@ const loggedOrNot = document.getElementById("logged-or-not");
 const emails = ["francesco@live.com", "marco@live.com", "andrea@live.com", "simone@live.com", "luca@live.com", "cristina@live.com",
 "anna@live.com", "antonio@live.com", "vanessa@live.com", "pamela@live.com", "dario@live.com", "christian@live.com", "francesco@gmail.com",];
 
+/* Prompt per inserire la propria mail */
 const userEmail = prompt("Inserisci la tua email:");
 
+/* Variabile di controllo corrispondenze */
 var emailsMatches = 0;
 
+/* Itero nell'array, se trovo corrispondenze "emailsMatches" viene incrementata di 1 */
 for (i = 0; i < emails.length; i++) {
     if (userEmail === emails[i]) {
         emailsMatches++;
     }
 }
-console.log(emailsMatches);
 
+/* Mostro un messaggio per confermare o negare la presenza dell'email in archivio */
 if (emailsMatches > 0) {
     loggedOrNot.innerHTML = "L'email fornita è registrata nel nostro archivio, puoi giocare scommettendo denaro.<br> Attenzione, il gioco d'azzardo crea sonnolenza.";
 } else {
